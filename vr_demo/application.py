d59f03999f4252db2dbe6f30984d237660b7caff
robinlin1973 <jielin88@hotmail.com>
2018-06-17 13:52:59 +0800

    add index page and change the ui logic

--- vr_demo/application.py
+++ vr_demo/application.py
@@ -1,28 +1,22 @@
 # coding: utf-8
 
-from flask import Flask, render_template,url_for,redirect
+from flask import Flask, render_template,url_for,redirect,request
 import boto3
+# from bs4 import BeautifulSoup
+# import requests
 
 application = Flask(__name__, template_folder="templates")
 
 #demo_vr_url = "<a href='http://m.detu.com/zh/pano/show/434339?from=singlemessage'>Loading VR</a>"
-demo_vr_url = "http://showroom.littleworkshop.fr/"#"http://m.detu.com/zh/pano/show/434339?from=singlemessage"
-sample_place_id = "ChIJ2YzT-RRLDW0RGqHdX0k4wJM"
+# demo_vr_url = "http://showroom.littleworkshop.fr/"#"http://m.detu.com/zh/pano/show/434339?from=singlemessage"
+# sample_place_id = "ChIJ2YzT-RRLDW0RGqHdX0k4wJM"
 s3 = boto3.resource('s3')
 s3_bucket_name = 'vr-content'
 vr_s3_folder = 'https://s3.us-east-2.amazonaws.com/vr-content/'
 bucket = s3.Bucket(s3_bucket_name)
 
-
-@application.route('/')
-def fullmap():
-    return render_template('google_search.html')
-
 @application.route("/fetch_vr/<string:place_id>")
 def fetch_vr(place_id):
-    # file_url = vr_s3_folder + sample_place_id + ".jpg"
-    # file_url = vr_s3_folder + place_id + ".jpg"
-
     key = place_id + '.jpg'
     objs = list(bucket.objects.filter(Prefix=key))
     if len(objs) > 0 and objs[0].key == key:
@@ -30,15 +24,32 @@ def fetch_vr(place_id):
         print("Exists! {}".format(file_url))
         return render_template("panolens.html", address=file_url)
     else:
-        print("Doesn't exist, show upload html")
+        # print("Doesn't exist, show upload html")
         return render_template("upload.html",place_id=place_id)
 
 
-@application.route("/upload")
-def upload_vr():
-    # print("show upload form")
-    return render_template("upload.html")
+# @application.route("/upload")
+# def upload_vr():
+#     # print("show upload form")
+#     return render_template("upload.html")
+
+@application.route("/",methods=['GET', 'POST'])
+def show_homepage():
+    return render_template("index.html")
+
+@application.route("/showmap")
+def show_map():
+    place_id = request.args.get('place_id')
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+    location = {
+        "place_id":place_id,
+        "lat":lat,
+        "lng":lng
+    }
+    return render_template("google_map.html",location=location)
+
 
 if __name__ == "__main__":
     #application.run(debug=True, use_reloader=True)
-    application.run(host="192.168.20.8")
+    application.run()#host="192.168.20.8"
